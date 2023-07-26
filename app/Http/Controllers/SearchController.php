7c67d335ce694136f7dffaf7a120df5df0160bce
funayamateppei <t.funayama0217@gmail.com>
2023-07-26 10:04:11 +0900

    DBから本日の天気情報を取得する関数を作成

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+
 use Illuminate\Support\Facades\Log;
 
 use App\Models\Prefecture;
@@ -15,11 +17,28 @@ class SearchController extends Controller
         return response()->json($data);
     }
 
-    // public function getRegions(Request $request)
-    // {
-    //     $prefecture_code = $request['prefecture_code'];
-    //     $data = Prefecture::with('regions')->where([['prefecture_code', $prefecture_code], []])->orderBy('id', 'asc')->get();
-    //     Log::debug($data);
-    //     return response()->json($data);
-    // }
+    public function getRegions(Request $request)
+    {
+        // 取得するデータが存在するかの確認
+        // $today = Carbon::today()->toDateString();
+        // $prefecture_code = $request['prefecture_code'];
+        // $data = Prefecture::with(['regions' => function ($query) use ($today) {
+        //     $query->whereDate('created_at', $today);
+        // }])->where('prefecture_code', $prefecture_code)->orderBy('id', 'asc')->get();
+
+        // 存在していればそのままデータを返却する
+
+        // 存在していなければ外部APIを呼び出し、取得してDBに保存する
+
+        // 今日の該当するデータをDBから取得する関数
+        $today = Carbon::today()->toDateString();
+        $prefecture_code = $request['prefecture_code'];
+        $data = Prefecture::with(['regions' => function ($query) use ($today) {
+            $query->whereDate('created_at', $today);
+        }])->where('prefecture_code', $prefecture_code)->orderBy('id', 'asc')->get();
+
+        // Log::debug($data);
+        // Log::debug($today);
+        return response()->json($data);
+    }
 }
